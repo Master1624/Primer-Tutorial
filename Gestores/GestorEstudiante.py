3641eee40735eeefeaf546c7dc91bb80c3d2819f
juanpalam <48248759+juanpalam@users.noreply.github.com>
2020-06-01 20:52:18 -0500

    Gestores

---
+++ Gestores/GestorEstudiante.py
@@ -0,0 +1,16 @@
+from tutorial import app, db
+
+def CrearEstudiante(ident, nombre, apellidos, carrera, foto):
+    pass
+
+def BuscarEstudiante(ident):
+    pass
+
+def VerEstudiantes():
+    pass
+
+def ModificarEstudiante(ident):
+    pass
+
+def EliminarEstudiante(ident):
+    pass
